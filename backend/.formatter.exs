63f8be4730bd510583117edf6e3a0d94f6e5331e
Guillaume Cugnet <guillaume@cugnet.eu>
2025-04-23 22:12:47 +0200

    feat(config): add 80 characters per lines constraint in formatter
    Refs: #4

--- backend/.formatter.exs
+++ backend/.formatter.exs
@@ -2,5 +2,10 @@
   import_deps: [:ash, :ash_json_api, :ash_postgres, :ecto, :ecto_sql, :phoenix],
   subdirectories: ["priv/*/migrations"],
   plugins: [Phoenix.LiveView.HTMLFormatter, Spark.Formatter],
-  inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}", "priv/*/seeds.exs"]
+  inputs: [
+    "*.{heex,ex,exs}",
+    "{config,lib,test}/**/*.{heex,ex,exs}",
+    "priv/*/seeds.exs"
+  ],
+  line_length: 80
 ]
