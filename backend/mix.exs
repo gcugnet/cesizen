24ade916193c201b18b066b097db55925b141baf
Guillaume Cugnet <guillaume@cugnet.eu>
2025-04-23 23:11:49 +0200

    deps(G:B): install 'ash_phoenix'
    Refs: #4

--- backend/mix.exs
+++ backend/mix.exs
@@ -33,6 +33,7 @@ defmodule Cesizen.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:ash_phoenix, "~> 2.0"},
       {:open_api_spex, "~> 3.0"},
       {:ash_json_api, "~> 1.0"},
       {:ash_postgres, "~> 2.0"},
