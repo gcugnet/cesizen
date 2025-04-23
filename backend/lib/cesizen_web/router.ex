03cd309dcd93cdc4d1cc7ae6493b7b00db53f098
Guillaume Cugnet <guillaume@cugnet.eu>
2025-04-23 23:00:30 +0200

    feat(B:Router): version the API
    Refs: #4

--- backend/lib/cesizen_web/router.ex
+++ backend/lib/cesizen_web/router.ex
@@ -14,11 +14,11 @@ defmodule CesizenWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/api/json" do
+  scope "/api/v1" do
     pipe_through [:api]
 
     forward "/swaggerui", OpenApiSpex.Plug.SwaggerUI,
-      path: "/api/json/open_api",
+      path: "/api/v1/open_api",
       default_model_expand_depth: 4
 
     forward "/", CesizenWeb.AshJsonApiRouter
