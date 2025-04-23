5d1386e92cc4ce0be1307e6dd24ed59a4d412dfb
Guillaume Cugnet <guillaume@cugnet.eu>
2025-04-23 20:52:49 +0200

    feat(config): add 'Spark.Formatter' plugin in formatter.exs
    Refs: #4

--- backend/.formatter.exs
+++ backend/.formatter.exs
@@ -1,6 +1,6 @@
 [
-  import_deps: [:ash_json_api, :ash_postgres, :ecto, :ecto_sql, :phoenix],
+  import_deps: [:ash, :ash_json_api, :ash_postgres, :ecto, :ecto_sql, :phoenix],
   subdirectories: ["priv/*/migrations"],
-  plugins: [Phoenix.LiveView.HTMLFormatter],
+  plugins: [Phoenix.LiveView.HTMLFormatter, Spark.Formatter],
   inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}", "priv/*/seeds.exs"]
 ]
