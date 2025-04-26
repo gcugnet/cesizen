5d64f7391b7de23321c24efbb2878191ad2368d3
Guillaume Cugnet <guillaume@cugnet.eu>
2025-04-26 11:52:46 +0200

    feat(B:D:Accounts): add code interfaces for User Resource
    Refs: #4

--- backend/lib/cesizen/accounts.ex
+++ backend/lib/cesizen/accounts.ex
@@ -1,7 +1,14 @@
+alias Cesizen.Accounts.User
+
 defmodule Cesizen.Accounts do
   use Ash.Domain, otp_app: :cesizen, extensions: [AshJsonApi.Domain]
 
   resources do
-    resource(Cesizen.Accounts.User)
+    resource User do
+      define :create_user, action: :create
+      define :list_users, action: :read
+      define :update_user, action: :update
+      define :delete_user, action: :destroy
+    end
   end
 end
