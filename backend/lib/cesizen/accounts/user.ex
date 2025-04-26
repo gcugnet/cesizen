265fce4103360313553fda910060a0dda69c43a3
Guillaume Cugnet <guillaume@cugnet.eu>
2025-04-26 11:43:27 +0200

    feat(B:R:User): update attributes and add unique 'email'
    Refs: #4

--- backend/lib/cesizen/accounts/user.ex
+++ backend/lib/cesizen/accounts/user.ex
@@ -27,22 +27,24 @@ defmodule Cesizen.Accounts.User do
   attributes do
     uuid_primary_key :id
 
-    attribute :name, :string do
-      allow_nil? false
-      public? true
-    end
+    attribute :email, :ci_string, allow_nil?: false, public?: true
+    attribute :name, :ci_string, allow_nil?: false, public?: true
 
     attribute :role, :atom do
       allow_nil? false
       constraints one_of: [:user, :admin]
       default :user
+      public? true
     end
 
     timestamps()
   end
 
+  identities do
+    identity :email, [:email]
+  end
+
   actions do
     defaults [:read, :destroy, create: :*, update: :*]
   end
-
 end
