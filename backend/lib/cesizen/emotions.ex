1ee6f7e41d815fc9d076a19118879188dd459193
Guillaume Cugnet <guillaume@cugnet.eu>
2025-05-19 15:03:17 +0200

    feat(B:D:Emotions): add code interfaces for Emotions and BasicEmotions
    Refs: #5

--- backend/lib/cesizen/emotions.ex
+++ backend/lib/cesizen/emotions.ex
@@ -1,8 +1,22 @@
 defmodule Cesizen.Emotions do
   use Ash.Domain, otp_app: :cesizen, extensions: [AshJsonApi.Domain]
 
+  alias Cesizen.Emotions.Emotion
+  alias Cesizen.Emotions.BasicEmotion
+
   resources do
-    resource Cesizen.Emotions.BasicEmotion
-    resource Cesizen.Emotions.Emotion
+    resource BasicEmotion do
+      define :list_basic, action: :read
+      define :create_basic, action: :create
+      define :update_basic, action: :update
+      define :delete_basic, action: :destroy
+    end
+
+    resource Emotion do
+      define :list, action: :list
+      define :create, action: :create
+      define :update, action: :update
+      define :delete, action: :destroy
+    end
   end
 end
