fe5d003792210eede6b6e2690efd86ae2fe1a4a1
Guillaume Cugnet <guillaume@cugnet.eu>
2025-05-20 09:33:34 +0200

    feat(B:D:Information): add code interfaces for Category and Content
    Refs: #10

--- backend/lib/cesizen/information.ex
+++ backend/lib/cesizen/information.ex
@@ -1,8 +1,22 @@
 defmodule Cesizen.Information do
   use Ash.Domain, otp_app: :cesizen, extensions: [AshJsonApi.Domain]
 
+  alias Cesizen.Information.Content
+  alias Cesizen.Information.Category
+
   resources do
-    resource Cesizen.Information.Category
-    resource Cesizen.Information.Content
+    resource Category do
+      define :list_categories, action: :read
+      define :create_category, action: :create
+      define :update_category, action: :update
+      define :delete_category, action: :destroy
+    end
+
+    resource Content do
+      define :list, action: :list
+      define :create, action: :create
+      define :update, action: :update
+      define :delete, action: :destroy
+    end
   end
 end
