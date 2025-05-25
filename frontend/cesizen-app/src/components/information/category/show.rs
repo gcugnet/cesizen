3dfe4a6744387fdf94b0083bd1d5672aff0a4cc7
Guillaume Cugnet <guillaume@cugnet.eu>
2025-05-25 19:53:42 +0200

    feat(F:S:Categories): category 'show' page displays a list of category's contents
    Refs: #18

--- frontend/cesizen-app/src/components/information/category/show.rs
+++ frontend/cesizen-app/src/components/information/category/show.rs
@@ -1,9 +1,10 @@
+use crate::utils::string::StringPreview;
 use cesizen_api::api::{
     information_category::InformationCategory, information_content::InformationContent, uuid::Uuid,
 };
-use dioxus::{logger::tracing::info, prelude::*};
+use dioxus::prelude::*;
 
-use crate::API;
+use crate::{Route, API};
 
 #[component]
 pub fn Show(id: Uuid) -> Element {
@@ -16,6 +17,58 @@ pub fn Show(id: Uuid) -> Element {
         use_resource(move || async move { InformationContent::list(&API.read(), Some(&id)).await });
 
     rsx! {
-        div { class: "h-full flex items-center justify-center", "Test" }
+        div { class: "h-full items-center justify-center",
+            match &*get_category.read_unchecked() {
+                Some(Ok(category)) => rsx! {
+                    div { class: "col-span-2 my-6 card w-full bg-base-100 card-xl shadow-sm",
+                        div { class: "card-body items-center text-center",
+                            div { class: "card-title", "{category.name()}" }
+                            if let Some(description) = category.description() {
+                                p { "{description}" }
+                            }
+                        }
+                    }
+                },
+                Some(Err(e)) => rsx! {
+                    div { "Erreur lors de la récupération des catégories : {e}" }
+                },
+                None => rsx! {
+                    div { "Chargement ..." }
+                },
+            }
+
+            match &*list_contents.read_unchecked() {
+                Some(Ok(contents)) => rsx! {
+                    // div { class: "container mx-auto px-4",
+                    div { class: "flex flex-wrap gap-6 justify-center",
+                        for content in contents.iter() {
+                            div { class: "mx-6 mb-6 card w-[40vw] bg-base-100 card-l shadow-sm",
+                                div { class: "card-body items-center text-center",
+                                    div { class: "card-title", "{content.title()}" }
+                                    p { "{content.body().preview(200)}" }
+                                    button {
+                                        class: "mt-2 btn btn-primary",
+                                        onclick: {
+                                            let id = *content.id();
+                                            move |_| {
+                                                nav.push(Route::Category { id });
+                                            }
+                                        },
+                                        "Voir"
+                                    }
+                                }
+                                    // }
+                            }
+                        }
+                    }
+                },
+                Some(Err(e)) => rsx! {
+                    div { "Erreur lors de la récupération des catégories : {e}" }
+                },
+                None => rsx! {
+                    div { "Chargement ..." }
+                },
+            }
+        }
     }
 }
