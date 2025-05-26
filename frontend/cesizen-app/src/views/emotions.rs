d15ed43c47cdf47d40a6c4049b07612edc2e8096
Guillaume Cugnet <guillaume@cugnet.eu>
2025-05-26 18:19:13 +0200

    feat(F:S:Emotions): implement a function to retrieve a user's emotions
    Refs: #19

--- frontend/cesizen-app/src/views/emotions.rs
+++ frontend/cesizen-app/src/views/emotions.rs
@@ -1,5 +1,6 @@
-use crate::API;
+use crate::{API, CURRENT_USER};
 use cesizen_api::api::emotion::Emotion;
+use cesizen_api::api::user::User;
 use dioxus::logger::tracing::info;
 use dioxus::prelude::*;
 
@@ -7,14 +8,15 @@ use dioxus::prelude::*;
 #[component]
 pub fn Emotions() -> Element {
     let mut basic_emotions = use_signal(|| None);
+    let mut user = use_signal(|| None);
 
     let get_basic_emotions = move |_| {
         spawn(async move {
             let resp = Emotion::list(&API.read()).await;
 
             match resp {
-                Ok(basic_emotions_response) => {
-                    basic_emotions.set(Some(basic_emotions_response));
+                Ok(response) => {
+                    basic_emotions.set(Some(response));
                 }
                 Err(e) => {
                     info!("Erreur {:?}", e);
@@ -23,7 +25,46 @@ pub fn Emotions() -> Element {
         });
     };
 
+    let get_user_emotions = move |_| {
+        let current_user = CURRENT_USER.cloned();
+        info!("{:?}", current_user);
+
+        spawn(async move {
+            let uuid = if let Some(user) = &current_user {
+                Some(user.id())
+            } else {
+                None
+            };
+
+            if let Some(id) = uuid {
+                let resp = User::get(&API.read(), id).await;
+
+                match resp {
+                    Ok(response) => {
+                        user.set(Some(response));
+                    }
+                    Err(e) => {
+                        info!("Erreur {:?}", e);
+                    }
+                }
+            } else {
+                info!("Error: CURRENT_USER uuid is None.")
+            }
+        });
+    };
+
     rsx! {
+        if let Some(user) = &*user.read() {
+            if let Some(emotions) = user.emotions() {
+                ul {
+                    for emotion in emotions.iter() {
+                        p { "{emotion.name()}" }
+                    }
+                }
+            }
+        }
+
+
         if let Some(basic_emotions) = &*basic_emotions.read() {
             ul {
                 for emotion in basic_emotions.iter() {
@@ -37,5 +78,7 @@ pub fn Emotions() -> Element {
         }
 
         button { class: "btn btn-primary", onclick: get_basic_emotions, "Get Basic Emotions" }
+
+        button { class: "btn btn-primary", onclick: get_user_emotions, "Get User Emotions" }
     }
 }
